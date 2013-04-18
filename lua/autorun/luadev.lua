40712edd02cb5000d8c1a8b7e71e30922016c0c5
Python1320 <Python1320@users.noreply.github.com>
2013-04-18 19:02:55 +0000

    fix

--- lua/autorun/luadev.lua
+++ lua/autorun/luadev.lua
@@ -3,7 +3,9 @@ module("luadev",package.seeall)
 -- I think I finally understood why people make these seemingly silly files with just includes
 
 include'luadev/luadev_sh.lua'
-include'luadev/luadev_sv.lua'
+if SERVER then
+	include'luadev/luadev_sv.lua'
+end
 include'luadev/luadev.lua'
 
 if SERVER then
