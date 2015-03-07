9d8c74ad7b9041c0fc98e8073273dea6bcca3419
Noah W <noiwex1911@gmail.com>
2015-03-07 12:45:12 +0000

    LuaDev client socket thing

--- lua/autorun/luadev.lua
+++ lua/autorun/luadev.lua
@@ -7,8 +7,12 @@ if SERVER then
 	include 'luadev/luadev_sv.lua'
 end
 include 'luadev/luadev.lua'
+if CLIENT then
+	include 'luadev/socketdev.lua'
+end
 
 if SERVER then
 	AddCSLuaFile 'luadev/luadev_sh.lua'
 	AddCSLuaFile 'luadev/luadev.lua'
+	AddCSLuaFile 'luadev/socketdev.lua'
 end
---
+++ lua/luadev/socketdev.lua
@@ -0,0 +1,52 @@
+if not luadev then
+	print"nah"
+	return
+end
+
+hook.Remove("Think", "LuaDev-Socket") -- upvalues will be lost
+
+collectgarbage()
+collectgarbage() -- finalizers will be scheduled for execution in the first pass, but will only execute in the second pass
+
+local ok, why = pcall(require, "luasocket")
+
+if not ok then
+	ErrorNoHalt(("\n\n\n\nUnable to load luasocket module (%s), LuaDev socket API will be unavailable\n\n\n\n"):format(tostring(why)))
+	return
+end
+
+local sock = socket.tcp()
+assert(sock:bind("127.0.0.1", 27099))
+sock:settimeout(0)
+assert(sock:listen(0))
+	
+local methods = {
+	self = luadev.RunOnSelf,
+	sv = luadev.RunOnServer,
+	sh = luadev.RunOnShared,
+	cl = luadev.RunOnClients
+}
+
+hook.Add("Think", "LuaDev-Socket", function()
+	local cl, a, b, c = sock:accept()
+	if cl then
+		system.FlashWindow()
+
+		if cl:getpeername() ~= "127.0.0.1" then
+			print("Refused", cl:getpeername())
+			cl:shutdown()
+			return
+		end
+
+		cl:settimeout(0)
+
+		local method = cl:receive("*l")
+		local who = cl:receive("*l")
+		local contents = cl:receive("*a")
+
+		if method and methods[method] then
+			methods[method](contents, who)
+		end
+		cl:shutdown()
+	end
+end)
