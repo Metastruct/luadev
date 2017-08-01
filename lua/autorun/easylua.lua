44f28c2895c63266a512413923d43e7ebcb28ab9
Datamats <datamats@datamats.com>
2017-08-01 21:38:04 +0200

    Add #(Country code) instead of #russians

--- lua/autorun/easylua.lua
+++ lua/autorun/easylua.lua
@@ -125,12 +125,21 @@ function easylua.FindEntity(str)
 		return friends
 	end
 
-	if str == "#russians" then
-		return russians
-	end
-
 	if str == "#randply" then
 		return table.Random(player.GetAll())
+	end
+
+	if str:sub(1,1) == "#" and #str == 3 then
+		local code = str:sub(2,3)
+		return CreateAllFunction(function()
+			local t = {}
+			for k,v in next,player.GetHumans() do
+				if (v.GetCountryCode and v:GetCountryCode() == code) then
+					t[#t+1] = v
+				end
+			end
+			return t
+		end)
 	end
 
 	if str:sub(1,1) == "#" then
@@ -727,15 +736,6 @@ do -- all
 		for k,v in next,player.GetHumans() do
 			if v == me then continue end
 			if (me.IsFriend and me:IsFriend(v) or (CLIENT and v:GetFriendStatus() == 'friend')) then
-				t[#t+1] = v
-			end
-		end
-		return t
-	end)
-	russians = CreateAllFunction(function()
-		local t = {}
-		for k,v in next,player.GetHumans() do
-			if (v.GetCountryCode and v:GetCountryCode() == "RU") then
 				t[#t+1] = v
 			end
 		end
