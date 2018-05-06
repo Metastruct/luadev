76f33a50240753740af858884a45136cdc37f356
nx <noiwex1911@gmail.com>
2018-05-06 10:26:37 +0300

    Revert "add #russians incase of emergency"
    This reverts commit d5817fd5458f1fbf2c5385c6fa2a83531796473b.

--- lua/autorun/easylua.lua
+++ lua/autorun/easylua.lua
@@ -129,10 +129,6 @@ function easylua.FindEntity(str)
 		return friends
 	end
 
-	if str == "#russians" then
-		return russians
-	end
-
 	if str == "#randply" then
 		return table.Random(player.GetAll())
 	end
@@ -737,15 +733,6 @@ do -- all
 		for k,v in next,player.GetHumans() do
 			if v == me then continue end
 			if (me.IsFriend and me:IsFriend(v) or (CLIENT and v:GetFriendStatus() == 'friend')) then
-				t[#t+1] = v
-			end
-		end
-		return t
-	end)
-	russians = CreateAllFunction(function()
-		local t = {}
-		for k,v in next,player.GetHumans() do
-			if (v.GetCountryCode and v:GetCountryCode() == "RU") then
 				t[#t+1] = v
 			end
 		end
