1acb735b16bfa726b2bd668900eb8e3b6e70fde2
Python1320 <python1320+gh@gmail.com>
2018-12-08 20:03:12 +0200

    Update luadev_sh.lua

--- lua/luadev/luadev_sh.lua
+++ lua/luadev/luadev_sh.lua
@@ -253,7 +253,7 @@ end
 		if !cl and easylua and easylua.FindEntity then
 			cl = easylua.FindEntity(plyid)
 		end
-		return IsValid(cl) and cl and cl:IsPlayer() or nil
+		return IsValid(cl) and cl:IsPlayer() and cl or nil
 	end
 	
 
