b291ef3c73cd8866af0b39414d4063b959204911
devSera <devsera.phend@gmail.com>
2020-05-10 16:13:27 -0400

    Implement map and filter into easylua all functions
    This was very fun <3
    
    WHY DOES VSCODE DO THIS AAA

--- lua/autorun/easylua.lua
+++ lua/autorun/easylua.lua
@@ -717,7 +717,22 @@ do -- all
 	local function pack(...)
 		return select('#', ...), {...}
 	end
+	
+	local function buildParser(input)
+		if isfunction(input) then return input end
+		local argStr, funcStr = input:match("(.-)->(.+)")
+		
+		if argStr and funcStr then
+			local codeFull = string.format("return function(%s) \n return %s \n end", argStr, funcStr)
+			local funcFactory = CompileString(codeFull, "ELua-FuncFactory", false)
+			
+			if funcFactory then
+				return funcFactory()
+			end
+		end
+	end
 
+	local INTERNAL = {}
 	local META = {}
 	
 	function META:__call()
@@ -727,6 +742,12 @@ do -- all
 	function META:__index(key)
 		if type(key) == "number" then
 			return self()[key]
+		end
+		
+		if INTERNAL[key] then
+			return function(_, ...)
+				return INTERNAL[key](self, ...)
+			end
 		end
 
 		return function(_, ...)
@@ -754,6 +775,8 @@ do -- all
 
 	function META:__newindex(key, value)
 		if type(key) == "number" then error"setting number index on entity" end
+		if INTERNAL[key] then  error"cannot set internal method index"  end
+
 		for _, ent in ipairs(self()) do
 			ent[key] = value
 		end
@@ -776,6 +799,42 @@ do -- all
 		end
 
 		return proxyObj
+	end
+
+	function INTERNAL:map(input)
+		local eval = buildParser(input)
+		local results = {}
+
+		if eval then
+			for _, ent in pairs(self())do
+				local response = eval(ent)
+
+				if response then
+					table.insert(results, response)
+				end
+			end
+		end
+
+		return CreateAllFunction(function()
+			return results
+		end)
+	end
+	
+	function INTERNAL:filter(input)
+		local eval = buildParser(input)
+		local results = {}
+
+		if eval then
+			for _, ent in pairs(self()) do
+				if eval(ent) then
+					table.insert(results, ent)
+				end
+			end
+		end
+
+		return CreateAllFunction(function()
+			return results
+		end)
 	end
 
 	all = CreateAllFunction(player.GetAll)
@@ -813,7 +872,8 @@ do -- all
 		end
 		return t
 	end)
-
+	
+	npcs = CreateAllFunction(function() return ents.FindByClass("npc_*") end)
 	props = CreateAllFunction(function() return ents.FindByClass("prop_physics") end)
 	these = CreateAllFunction(function() return constraint.GetAllConstrainedEntities(_G.this) end)
 	those = CreateAllFunction(function() return ents.FindInSphere(_G.there, 250) end)
